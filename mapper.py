0e6ca66f379d88766b3de74c5f7d0f5a7b485f98
auliapakpahan <55121424+auliapakpahan@users.noreply.github.com>
2021-02-28 16:18:29 +0700

    final commit

---
+++ mapper.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""mapper.py"""
+
+import sys
+
+for line in sys.stdin:
+ 
+    line = line.strip()
+   
+    words = line.split()
+ 
+    for word in words:
+       
+        print ('%s\t%s' % (word, 1))
